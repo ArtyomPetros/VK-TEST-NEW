8050536ae6d81f87d10dc86b41003f768c8f7a23
PetrosyanARTYOM <petrosyanartyomm@gmail.com>
2024-12-04 16:15:04 +0300

    Initial Commit

---
+++ VK TEST NEW/VK_TEST_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_TEST_NEWApp.swift
+//  VK TEST NEW
+//
+//  Created by Артём Петросян on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct VK_TEST_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
